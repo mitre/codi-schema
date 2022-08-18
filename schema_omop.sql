94b8937e1471887c68994dec924df1a821a7e5b7
radamson <radamson@users.noreply.github.com>
2022-08-17 20:54:22 -0400

    update cost table

--- schema_omop.sql
+++ schema_omop.sql
@@ -2,12 +2,12 @@ CREATE SCHEMA OMOP;
 
 CREATE TABLE omop.cost
 (
-    cost_id character varying(255) PRIMARY KEY,
-    person_id character varying(255) NOT NULL,
-    cost_event_id character varying(255),
-    cost_event_field_concept_id character varying(255),
-    cost_concept_id character varying(255) ,
-    cost numeric NOT NULL,
+    cost_id varchar PRIMARY KEY,
+    person_id varchar NOT NULL,
+    cost_event_id varchar NOT NULL,
+    cost_event_field_concept_id varchar NOT NULL,
+    cost_concept_id varchar NOT NULL,
+    cost float NOT NULL,
     incurred_date date NOT NULL,
     billed_date date,
     paid_date date
